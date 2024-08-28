e0530baa9e1f9dceb35165eadfc319af3dea1231
Sathvik-pechetty <166890139+Sathvik-pechetty@users.noreply.github.com>
2024-08-28 10:19:22 +0530

    static variable.java

---
+++ static variable.java
@@ -0,0 +1,22 @@
+public class one{
+		static int i;
+		static float f;
+		static boolean o;
+		static double d;
+		static char c;
+		static byte b;
+		static short s;
+		static long l;
+	public static void main(String[] args){
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(o);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(l);
+		
+	}
+
+}
